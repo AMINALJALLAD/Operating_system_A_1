c861cb6744640877a8c030d5dc2449f757f477d5
Amin Aljallad <a.aljallad1989@hotmail.com>
2020-02-04 11:20:45 -0500

    adding files

---
+++ src/Main_Class.java
@@ -0,0 +1,35 @@
+//package comp346pa1w2020;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//import java.lang.*;
+import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+public class Main_Class {
+
+    public static void main(String[] args) {
+    	FileOutputStream f = null;
+    	try {
+    		f = new FileOutputStream("Output.txt");
+    	}catch(FileNotFoundException e) {
+    		System.out.println("File not found");
+    		System.exit(0);
+    	}
+    	System.setOut(new PrintStream(f));
+        Network objNetwork = new Network("network");            /* Activate the network */
+        objNetwork.start();
+        Server objServer = new Server();                        /* Start the server */
+        objServer.start();
+        Client objClient1 = new Client("sending");              /* Start the sending client */
+        objClient1.start();
+        Client objClient2 = new Client("receiving");            /* Start the receiving client */
+        objClient2.start();
+
+    }
+
+}
